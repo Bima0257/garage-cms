7d6e37bb324166dd5206a8951949edaf8db5137f
Bima0257 <bimatri377@gmail.com>
2026-06-19 10:01:34 +0700

    fix: update default image path to /default-img/default.jpg

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-export function safeImageSrc(image?: string | null, defaultImg = '/default.jpg'): string {
+export function safeImageSrc(image?: string | null, defaultImg = '/default-img/default.jpg'): string {
   if (!image) return defaultImg
   if (image.startsWith('http://') || image.startsWith('https://') || image.startsWith('/')) return image
   return '/' + image
